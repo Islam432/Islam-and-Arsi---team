7bab6f2318d0961914911b90785cfb3c65790d00
Islam Temirgaliev <islam2004tr@mail.ru>
2023-03-12 00:30:42 +0600

    navbar-costomisation1

---
+++ .history/src/components/Navbar/Navbar_20230311235937.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+  return (
+    <div className={styles.cont}>
+      <form className={styles.allnav}>
+       <div className={l}> <img className={styles.logo-img} src="./Logo.png" alt="" /> <h1>Logo</h1></div>
+
+        <div className={styles.navbar}>
+          <NavLink to={'/'} className={styles.NavLink}>
+            <h3>home</h3>
+          </NavLink>
+          <NavLink to={'/comp'} className={styles.NavLink}>
+            <h3>Navbar</h3>
+          </NavLink>
+          <NavLink className={styles.NavLink}>
+            <h3>Navbar</h3>
+          </NavLink>
+          <NavLink className={styles.NavLink}>
+            <h3>Navbar</h3>
+          </NavLink>
+         
+        </div>
+
+        <div className={styles.inputblok}>
+          <input type="text" placeholder="serch" />
+        </div>
+
+        <div>basket</div>
+      </form>
+    </div>
+  );
+};
---
+++ .history/src/components/Navbar/Navbar_20230312000251.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+  return (
+    <div className={styles.cont}>
+      <form className={styles.allnav}>
+       <div className={styles.logo}> <div></div> <h1>Logo</h1></div>
+
+        <div className={styles.navbar}>
+          <NavLink to={'/'} className={styles.NavLink}>
+            <h3>home</h3>
+          </NavLink>
+          <NavLink to={'/comp'} className={styles.NavLink}>
+            <h3>Navbar</h3>
+          </NavLink>
+          <NavLink className={styles.NavLink}>
+            <h3>Navbar</h3>
+          </NavLink>
+          <NavLink className={styles.NavLink}>
+            <h3>Navbar</h3>
+          </NavLink>
+         
+        </div>
+
+        <div className={styles.inputblok}>
+          <input type="text" placeholder="serch" />
+        </div>
+
+        <div>basket</div>
+      </form>
+    </div>
+  );
+};
